09d5e13ebeda714bc3a982c3ccb20500b2e54357
Adelson Santos <adelson.art@hotmail.com>
2018-08-22 09:06:36 -0300

    add formulario dos dados pessoais

---
+++ views/dados-unico-funcionario/_form-dados-pessoais.php
@@ -0,0 +1,48 @@
+
+    <?= $form->field($model, 'pessoa_id')->textInput() ?>
+
+    <?= $form->field($model, 'funcionario_tipo_id')->textInput() ?>
+
+    <?= $form->field($model, 'funcao_id')->textInput() ?>
+
+    <?= $form->field($model, 'cargo_permanente')->textInput() ?>
+
+    <?= $form->field($model, 'funcionario_matricula')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'funcionario_ramal')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'funcionario_email')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'funcionario_dt_admissao')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'funcionario_dt_demissao')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'funcionario_orgao_origem')->textInput() ?>
+
+    <?= $form->field($model, 'funcionario_conta_fgts')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'contrato_id')->textInput() ?>
+
+    <?= $form->field($model, 'funcionario_salario')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'cargo_temporario')->textInput() ?>
+
+    <?= $form->field($model, 'funcionario_orgao_destino')->textInput() ?>
+
+    <?= $form->field($model, 'est_organizacional_lotacao_id')->textInput() ?>
+
+    <?= $form->field($model, 'funcionario_validacao_propria')->textInput() ?>
+
+    <?= $form->field($model, 'funcionario_validacao_rh')->textInput() ?>
+
+    <?= $form->field($model, 'funcionario_envio_email')->textInput() ?>
+
+    <?= $form->field($model, 'funcionario_tipo_id_old')->textInput() ?>
+
+    <?= $form->field($model, 'motorista')->textInput() ?>
+
+    <?= $form->field($model, 'funcionario_onus')->textInput(['maxlength' => true]) ?>
+
+
+
+
